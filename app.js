3aebe88f8b3aa356fa14f7d1cd221bf39fdd790f
roshin Y S <roshinchaya@gmail.com>
2022-09-30 08:53:22 +0530

    assignment with routes

--- app.js
+++ app.js
@@ -1,42 +1,7 @@
-const { rmSync } = require("fs");
 const http = require("http");
+const routeHandler = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Enter User</title><head>");
-    res.write(
-      '<body><form action="/createUser" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html/>");
-    return res.end();
-  }
-  if (url === "/users") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Enter User</title><head>");
-    res.write("<body><ul><li>user 1</li><li>user 2</li></ul></body>");
-    res.write("</html/>");
-    return res.end();
-  }
-  if (url === "/createUser" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    req.on("close", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody.split("=")[1]);
-    });
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-});
+const server = http.createServer(routeHandler);
 server.listen(3000, () => {
   console.log("server listening on port 3000");
 });
